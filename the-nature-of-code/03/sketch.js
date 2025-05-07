0037d8987d81b7fde6e5c3244f6a30ee6e70539f
Reagan Chisholm <reaganchisholm@gmail.com>
2025-05-06 21:59:29 -0400

    springs

--- the-nature-of-code/03/sketch.js
+++ the-nature-of-code/03/sketch.js
@@ -1,54 +1,133 @@
-let osc = [];
-let startAngle = 0;
-let deltaAngle = 0.2;
-let amplitude = 100;
+let bob;
+let spring;
 
 function setup() {
   createCanvas(400, 400);
 
+  spring = new Spring(width / 2, 10, 100);
+  bob = new Bob(width / 2, 100);
 }
 
 function draw() {
   background(255);
-  let angle = startAngle;
-  for (let x = 0; x < width; x += 3) {
-    let y = map(tan(angle), -1, 1, 0, height);
-    stroke(0);
-    fill(255);
-    circle(x, y, 48);
-    angle += deltaAngle;
-  }
-  for (let x = 0; x < width; x += 5) {
-    let y = map(sin(angle), -1, 1, 0, height);
-    stroke(0);
-    fill(0);
-    circle(x, y, 48);
-    angle += deltaAngle;
-  }
-  startAngle += 0.02;
+
+  let gravity = createVector(0, 2);
+
+  bob.applyForce(gravity);
+  bob.update();
+  bob.handleDrag(mouseX, mouseY);
+
+  spring.connect(bob);
+  spring.constrainLength(bob, 30, 400);
+
+  spring.showLine(bob);
+  bob.show();
+  spring.show();
 }
 
-class Oscillator {
-  constructor() {
-    this.angle = createVector();
-    this.angleVelocity = createVector(random(-0.05, 0.05), random(-0.05, 0.05));
-    this.amplitude = createVector(random(20, width / 2), random(20, height / 2));
+function mousePressed() {
+  bob.handleClick(mouseX, mouseY);
+}
+
+function mouseReleased() {
+  bob.stopDragging();
+}
+
+class Bob {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector();
+    this.acceleration = createVector();
+    this.mass = 24;
+    this.damping = 0.98;
+    this.dragOffset = createVector();
+    this.dragging = false;
   }
 
   update() {
-    this.angle.add(this.angleVelocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.mult(this.damping);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  // Newton's law: F = M * A
+  applyForce(force) {
+    let f = force.copy();
+    f.div(this.mass);
+    this.acceleration.add(f);
   }
 
   show() {
-    let x = sin(this.angle.x) * this.amplitude.x;
-    let y = sin(this.angle.y) * this.amplitude.y;
-
-    push();
-    translate(width / 2, height / 2);
     stroke(0);
+    strokeWeight(2);
     fill(127);
-    line(0, 0, x, y);
-    circle(x, y, 32);
-    pop();
+    if (this.dragging) {
+      fill(200);
+    }
+    circle(this.position.x, this.position.y, this.mass * 2);
+  }
+
+  handleClick(mx, my) {
+    let d = dist(mx, my, this.position.x, this.position.y);
+    if (d < this.mass) {
+      this.dragging = true;
+      this.dragOffset.x = this.position.x - mx;
+      this.dragOffset.y = this.position.y - my;
+    }
+  }
+
+  stopDragging() {
+    this.dragging = false;
+  }
+
+  handleDrag(mx, my) {
+    if (this.dragging) {
+      this.position.x = mx + this.dragOffset.x;
+      this.position.y = my + this.dragOffset.y;
+    }
+  }
+}
+
+
+class Spring {
+  constructor(x, y, length) {
+    this.anchor = createVector(x, y);
+    this.restLength = length;
+    this.k = 0.2;
+  }
+
+  connect(bob) {
+    let force = p5.Vector.sub(bob.position, this.anchor);
+    let currentLength = force.mag();
+    let stretch = currentLength - this.restLength;
+
+    force.setMag(-1 * this.k * stretch);
+    bob.applyForce(force);
+  }
+
+  constrainLength(bob, minlen, maxlen) {
+    let direction = p5.Vector.sub(bob.position, this.anchor);
+    let length = direction.mag();
+
+    if (length < minlen) {
+      direction.setMag(minlen);
+      bob.position = p5.Vector.add(this.anchor, direction);
+      bob.velocity.mult(0);
+    } else if (length > maxlen) {
+      direction.setMag(maxlen);
+      bob.position = p5.Vector.add(this.anchor, direction);
+      bob.velocity.mult(0);
+    }
+  }
+
+  show() {
+    fill(127);
+    circle(this.anchor.x, this.anchor.y, 10);
+  }
+
+  showLine(bob) {
+    stroke(0);
+    line(bob.position.x, bob.position.y, this.anchor.x, this.anchor.y);
   }
 }
