4a69c5c71d845bb28a83436b34ba602d84cdd085
Reagan Chisholm <reaganchisholm@gmail.com>
2025-08-13 08:05:47 -0400

    07 - sierpinski triangle

--- the-nature-of-code/07/sketch.js
+++ the-nature-of-code/07/sketch.js
@@ -1,7 +1,41 @@
+let cells;
+let generation = 0;
+let w = 10;
+
+let ruleset = [0, 1, 0, 1, 1, 0 , 1, 0];
+
 function setup() {
-  let canvas = createCanvas(500, 500);
+  createCanvas(500, 500);
+  background(255);
+
+  // Set up cells
+  cells = new Array(floor(width / w));
+  for (let i = 0; i < cells.length; i++){ cells[i] = 0 }
+  cells[floor(cells.length/2)] = 1;
 }
 
 function draw() {
-  background(255);
+  let nextgen = cells.slice();
+  
+  for (let i = 1; i < cells.length - 1; i++){
+    if(cells[i] === 1){
+      fill(0);
+      square(i * w, generation * w, w);
+    }
+
+    let a = cells[i - 1];
+    let b = cells[i];
+    let c = cells[i + 1];
+
+    nextgen[i] = rules(a, b, c);
+  }
+
+  cells = nextgen;
+  generation++;
+}
+
+function rules(a, b, c){
+  let s = "" + a + b + c;
+  let index = parseInt(s, 2);
+  return ruleset[7 - index];
 }
