4d655b68f813b1f2f570e374c16bd2c194607b03
Reagan Chisholm <reaganchisholm@gmail.com>
2025-08-19 08:01:32 -0400

    koch

--- the-nature-of-code/08/sketch-koch-line.js
+++ the-nature-of-code/08/sketch-koch-line.js
@@ -7,6 +7,10 @@ function setup() {
   let start = createVector(0, height/2);
   let end = createVector(width, height/2)
   segments.push(new KochLine(start, end));
+
+  for (let i = 0; i < 5; i++){
+    generate();
+  }
 }
 
 function draw() {
@@ -16,6 +20,17 @@ function draw() {
 }
 
 function generate(){
+  let next = [];
+
+  for(let segment of segments){
+    const [a, b, c, d, e] = segment.kochPoints();
+    next.push(new KochLine(a, b));
+    next.push(new KochLine(b, c));
+    next.push(new KochLine(c, d));
+    next.push(new KochLine(d, e));
+  }
+
+  segments = next;
 }
 
 class KochLine {
@@ -27,5 +42,17 @@ class KochLine {
   show(){
     stroke(0);
     line(this.start.x, this.start.y, this.end.x, this.end.y);
+  }
+
+  kochPoints(){
+    let a = this.start.copy();
+    let e = this.end.copy();
+    let v = p5.Vector.sub(this.end, this.start);
+    v.div(3);
+    let b = p5.Vector.add(a, v);
+    let d = p5.Vector.add(b, v);
+    v.rotate(-PI / 3);
+    let c = p5.Vector.add(b, v);
+    return [a, b, c, d, e];
   }
 }
